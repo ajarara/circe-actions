951ef8d8302f8ea9a5b454322f98be78111fc9a8
Ahmad Jarara <floor13@jarmac.org>
2016-12-13 17:02:50 -0500

    experimental macro manipulation
    May not be the right thing for the job.
    
    Macros are expanded at definition time. I want a function that is
    built everytime I call control-reply-gen, with parameters as functions
    that I supply to it.

--- zncirce.el
+++ zncirce.el
@@ -35,7 +35,9 @@ handler error out. This is unset after the function removes
 itself, or when you call zncirce-remove-control-reply (which
 removes the function for you")
 
+;; this no longer works
 (defun zncirce-remove-control-reply ()
+  " This no longer works!! "
   (interactive)
   (irc-handler-remove (circe-irc-handler-table)
 		      "irc.message"
@@ -56,19 +58,54 @@ generated function."
                         #'zncirce-control-reply)
     (setq zncirce-command-mutex nil)))
 
-  
-(defun zncirce-controlpanel-wrap (func-to-wrap)
+(defmacro zncirce-control-reply-gen (condition action symbol)
+  "Given a condition and action, both lambda expressions that take five arguments:
+- server-proc
+- event
+- fq-username
+- channel
+- contents
+return a function that is designed to be placed on circe's irc-message event.
+
+This generated function has the following responsibilities:
+When called, pass the arguments to the condition. When the condition returns true, then pass the arguments to the action.
+
+Then remove itself off circe-irc-handler-table's 'irc.message' bucket, and unset the mutex. (in the future, remove itself off the list of active handlers)
+
+The symbol should be uninterned, but doesn't have to be."
+  `(defun ,symbol (server-proc event fq-username channel contents)
+     (when (condition
+		    server-proc event fq-username channel contents)
+       (action
+		server-proc event fq-username channel contents)
+       (irc-handler-remove (circe-irc-handler-table)
+			   "irc.message"
+			   ,symbol)
+       (setq zncirce-command-mutex nil))))
+
+;; (defun zncirce-control-reply-gen (condition action symbol)
+;;   (defun ,symbol (server-proc event fq-username channel contents)
+;;     (when (funcall condition
+;; 		   server-proc event fq-username channel contents)
+;;       (funcall action
+;; 	       server-proc event fq-username channel contents)
+;;       (irc-handler-remove (circe-irc
+      
+   
+(defun zncirce-controlpanel-wrap (func-to-wrap condition action)
   "given a function, add the zncirce-control-reply handler to circe's
 irc.message event, and execute the function. zncirce-control-reply waits for a
 message (in this case, anything that we get back from *controlpanel),
 removes itself, and the function "
   (if zncirce-command-mutex
       (message "Currently waiting for a message from *controlpanel. Try again later or remove the circe handler with zncirce-remove-control-reply")
-    (progn
+    (let ((symbol (gensym "zncirce-sym")))
       (setq zncirce-command-mutex t)
       (irc-handler-add (circe-irc-handler-table)
-                       "irc.message"
-                       #'zncirce-control-reply)
+		       "irc.message"
+		       (zncirce-control-reply-gen 'condition
+						  'action
+						  symbol))
       (funcall func-to-wrap))))
 
 (defun zncirce-get-buffer-for-chan (buf &optional arg)
@@ -86,7 +123,7 @@ take out the querying *controlpanel functionality in this code, move it to it's 
   (if (= arg 4)
       ;; arg is set, set variable for the channel instead.
       (let ((buffervar (read-number "Number of messages to buffer: ")))
-	(zncirce-controlpanel-wrap
+	(zncirce-controlpanel-wrap 
 	 (lambda ()
 	   (circe-command-MSG "*controlpanel"
 			      (concat "setchan buffer $me "
@@ -94,11 +131,25 @@ take out the querying *controlpanel functionality in this code, move it to it's 
 				      " "
 				      buf
 				      " "
-				      (number-to-string buffervar))))))
+				      (number-to-string buffervar))))
+	 'zncirce-from-controlpanelp
+	 'zncirce-message-contents
+	 ))
     (zncirce-controlpanel-wrap
      (lambda ()
        (circe-command-MSG "*controlpanel"
 			  (concat "getchan buffer $me "
 				  (funcall zncirce-server-name)
 				  " "
-				  buf))))))
+				  buf)))
+     'zncirce-from-controlpanelp
+     'zncirce-message-contents)))
+
+;; examples of conditions
+(defun zncirce-from-controlpanelp (server-proc event fq-username channel contents)
+  (equal fq-username "*controlpanel!znc@znc.in"))
+
+;; examples of actions
+(defun zncirce-message-contents (server-proc event fq-username channel contents)
+  (message contents))
+
