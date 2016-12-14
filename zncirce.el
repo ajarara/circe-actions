e5ecdfae0b406e1d5aa48597d437c8622661ec46
Ahmad Jarara <floor13@jarmac.org>
2016-12-14 16:23:39 -0500

    Remove specific code, begin alist experimentation

--- zncirce.el
+++ zncirce.el
@@ -13,6 +13,16 @@
 ;; shell access ZNC gives admins.
 
 ;; not implemented
+;; (defgroup zncirce nil
+;;   :group 'convenience)
+
+;; (defcustom zncirce-maximum-commands 1
+;;   "Wait for n responses before allowing the user to send another command.
+;;    This number is compared against zncirce-command-alist's length. Once it is greater than or equal to the length of the alist, ignore any requests to add more to the queue."
+;;   :group 'zncirce
+;;   :type 'integer)
+
+
 ;; (defcustom zncirce-enable-command-mutex t
 ;;    "Wait for responses before allowing the user to send another command.
 ;;     If a new command is attempted while waiting for a reply from the 
@@ -20,10 +30,10 @@
 ;;     :type 'boolean
 ;;     :group 'zncirce)
 
-
 ;; Hmm. Unfortunately it is difficult to figure out how to dynamically
 ;; guess what server you're on. IDK how to do it. For now I will wrap
 ;; this in a lambda and just return freenode.
+;; ZNC unfortunately does not provide for an irc-level mechanism to tell what server you are currently talking to (sort of.)
 (defvar zncirce-server-name
     (lambda ()
       "freenode"))
@@ -35,63 +45,24 @@ handler error out. This is unset after the function removes
 itself, or when you call zncirce-remove-control-reply (which
 removes the function for you")
 
-;; this no longer works
-(defun zncirce-remove-control-reply ()
-  " This no longer works!! "
-  (interactive)
-  (irc-handler-remove (circe-irc-handler-table)
-		      "irc.message"
-		      #'zncirce-control-reply)
-  (setq zncirce-command-mutex nil)
-  (message "Unset circe-znc-mutex"))
 
-(defun zncirce-control-reply (server-proc event fq-username channel contents)
-  "this is put on circe's irc-handler-table, reads the next event from
-,*controlpanel, displays it in the minibuffer, and then pops itself
-off. Can easily be replaced with a function that does something else,
-(ie by using a macro) if I could figure out how to refer to the
-generated function."
-  (when (equal fq-username "*controlpanel!znc@znc.in")
-    (message contents)
-    (irc-handler-remove (circe-irc-handler-table)
-                        "irc.message"
-                        #'zncirce-control-reply)
-    (setq zncirce-command-mutex nil)))
-
-(defmacro zncirce-control-reply-gen (condition action symbol)
-  "Given a condition and action, both lambda expressions that take five arguments:
-- server-proc
-- event
-- fq-username
-- channel
-- contents
-return a function that is designed to be placed on circe's irc-message event.
-
-This generated function has the following responsibilities:
-When called, pass the arguments to the condition. When the condition returns true, then pass the arguments to the action.
-
-Then remove itself off circe-irc-handler-table's 'irc.message' bucket, and unset the mutex. (in the future, remove itself off the list of active handlers)
-
-The symbol should be uninterned, but doesn't have to be."
-  `(defun ,symbol (server-proc event fq-username channel contents)
-     (when (condition
-		    server-proc event fq-username channel contents)
-       (action
-		server-proc event fq-username channel contents)
-       (irc-handler-remove (circe-irc-handler-table)
-			   "irc.message"
-			   ,symbol)
-       (setq zncirce-command-mutex nil))))
-
-;; (defun zncirce-control-reply-gen (condition action symbol)
-;;   (defun ,symbol (server-proc event fq-username channel contents)
-;;     (when (funcall condition
-;; 		   server-proc event fq-username channel contents)
-;;       (funcall action
-;; 	       server-proc event fq-username channel contents)
-;;       (irc-handler-remove (circe-irc
+;; FIX ME
+;; Why doesn't this work?
+;; void-function "condition"
+;; dynamic lexical scoping issue?
+(defun zncirce-control-reply-gen (condition action)
+  (let ((some-symbol (gensym "zncirce-sym")))
+    (defalias some-symbol
+      (lambda (server-proc event fq-username channel contents)
+	(when (funcall condition
+		       server-proc event fq-username channel contents)
+	  (funcall action
+		   server-proc event fq-username channel contents)
+	  (irc-handler-remove (circe-irc-handler-table)
+			      "irc.message"
+			      some-symbol)
+	  (setq zncirce-command-mutex nil))))))
       
-   
 (defun zncirce-controlpanel-wrap (func-to-wrap condition action)
   "given a function, add the zncirce-control-reply handler to circe's
 irc.message event, and execute the function. zncirce-control-reply waits for a
@@ -99,14 +70,21 @@ message (in this case, anything that we get back from *controlpanel),
 removes itself, and the function "
   (if zncirce-command-mutex
       (message "Currently waiting for a message from *controlpanel. Try again later or remove the circe handler with zncirce-remove-control-reply")
-    (let ((symbol (gensym "zncirce-sym")))
-      (setq zncirce-command-mutex t)
-      (irc-handler-add (circe-irc-handler-table)
-		       "irc.message"
-		       (zncirce-control-reply-gen 'condition
-						  'action
-						  symbol))
-      (funcall func-to-wrap))))
+    (setq zncirce-command-mutex t)
+    (irc-handler-add (circe-irc-handler-table)
+		     "irc.message"
+		     (zncirce-control-reply-gen 'condition
+						'action))
+    (funcall func-to-wrap)))
+
+(defun zncirce-panic ()
+  "Remove all irc.message handlers, unset the mutex, pray to RMS"
+  (interactive)
+  (setq zncirce-command-mutex nil)
+  (puthash "irc.message"
+	   '()
+	   (circe-irc-handler-table))
+  )
 
 (defun zncirce-get-buffer-for-chan (buf &optional arg)
   "Query *controlpanel for the buffer variable for a specific channel
@@ -148,6 +126,7 @@ take out the querying *controlpanel functionality in this code, move it to it's 
 ;; examples of conditions
 (defun zncirce-from-controlpanelp (server-proc event fq-username channel contents)
   (equal fq-username "*controlpanel!znc@znc.in"))
+
 
 ;; examples of actions
 (defun zncirce-message-contents (server-proc event fq-username channel contents)
