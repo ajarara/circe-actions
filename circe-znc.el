32efac97a3505ca076e190bf4e0f0053c443a4f3
Ahmad Jarara <ajarara@jarmac.org>
2017-03-01 18:54:07 -0500

    add help-sentinel logic
    I'm not sure if i want to try playing around with buttercup's finicky
    lexical binding testing rn.

--- circe-znc.el
+++ circe-znc.el
@@ -16,6 +16,7 @@
 (require 'circe-actions)
 (require 'subr-x)
 
+
 (defvar circe-znc-status-table
   (let ((hash-table (make-hash-table :test #'equal)))
     (puthash "help" (lambda () (message "Test passed!")) hash-table))
@@ -23,8 +24,9 @@
 
 
 (defvar circe-znc-collect-timeout 10
-  "Number of seconds to wait before deactivating listener for some ZNC output.
-Raise this if output is cut off (ie missing help messages)")
+  "Number of seconds to wait before deactivating listener for some
+  extended ZNC output (AKA to a buffer).  Raise this if output is cut
+  off (ie missing help messages). Lower this if subsequent in")
 
 (defvar circe-znc-controlpanel-table
   (let ((hash-table (make-hash-table :test #'equal)))
@@ -33,9 +35,9 @@ Raise this if output is cut off (ie missing help messages)")
   "")
 
 
-;; ;; it's kind of silly to make this a hash table if I query for the list everytime.
-;; ;; the only time I would keep it a hash table is if the results were themselves
-;; ;; interactive functions.
+;; it's kind of silly to make this a hash table if I query for the list everytime.
+;; the only time I would keep it a hash table is if the results were themselves
+;; interactive functions.
 (defvar circe-znc-modules-table
   (let ((hash-table (make-hash-table :test #'equal)))
     (puthash "*controlpanel" circe-znc-controlpanel-table hash-table)
@@ -65,25 +67,49 @@ Raise this if output is cut off (ie missing help messages)")
     ;; pop-to-buffer instead?
     (display-buffer buffer)))
 
- (defun circe-znc--get-buffer-create (bufname)
-   "If circe-znc--output-stale is set, clear the buffer.
+(defun circe-znc--get-buffer-create (bufname)
+  "If circe-znc--output-stale is set, clear the buffer.
  This means that the associated handler has been deactivated!
  (An alternate implementation might be to associate the symbol as a
  buffer-local variable and simply check if the assoc handler is
  deactivated.)
  Otherwise simply return the buffer."
-   (if (get-buffer bufname) ; returns nil if it doesn't exist or killed
-       (with-current-buffer bufname
-         (if (funcall circe-znc--is-live-p) ; is buffer's handler still alive?
-             bufname 
-           (kill-buffer bufname)
-           (circe-znc--get-buffer-create bufname)))
-     (let ((newbuf (generate-new-buffer bufname)))
-       (with-current-buffer newbuf
-         (circe-znc-output-mode)
-         (setq-local circe-znc--is-live-p (lambda () nil)) ; initialize live-p
-         newbuf))))
+  (if (get-buffer bufname) ; returns nil if it doesn't exist or killed
+      (with-current-buffer bufname
+        (if (funcall circe-znc--is-live-p) ; is buffer's handler still alive?
+            bufname 
+          (kill-buffer bufname)
+          (circe-znc--get-buffer-create bufname)))
+    (let ((newbuf (generate-new-buffer bufname)))
+      (with-current-buffer newbuf
+        (circe-znc-output-mode)
+        (setq-local circe-znc--is-live-p (lambda () nil)) ; initialize live-p
+        newbuf))))
        
+
+(defvar circe-znc--help-sentinels
+  (let ((table (make-hash-table :test #'equal)))
+    (puthash "*controlpanel" '(:re "^+=\+\\+=\+\\+$" :times 3) table) ; thank computer jesus for re-builder
+    (puthash "test" '(:re "5" :times 3) table)
+    table)
+  "set of conditions that determine if help output has ceased. As of now only supports :re and :times as conditions, eventual support for custom timeouts.")
+
+(defun circe-znc--deactivation-p-gen (module-name)
+  "given a module-name, look up the sentinel in
+  `circe-znc--help-sentinels' and generate a closure for it, returning
+  true when the sentinel's :re and :times is satisfied.
+
+Planned: generate get time of closure generation. If time differs by circe-znc-collect-timeout, return true."
+  (let* ((sentinel (gethash module-name circe-znc--help-sentinels))
+         (re (plist-get sentinel :re))
+         (times (plist-get sentinel :times))
+         (matches 0))
+    (lambda (string)
+      (if (string-match-p re string)
+          (if (<= times matches)
+              t
+            (setq matches (1+ matches)))))))
+
 (define-derived-mode circe-znc-output-mode
   special-mode
   "*ZNC Command Output*"
